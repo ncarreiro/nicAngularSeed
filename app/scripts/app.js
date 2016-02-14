cc2ae917b8ea2c17b0fd3535f8bb8e8b011c8cf0
ncarreiro <nahuel.c@geopagos.com>
2016-02-14 14:53:30 -0300

    - First commit of whole seed files.

---
+++ app/scripts/app.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name miPromoApp
+ * @description
+ * # miPromoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('miPromoApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ngMaterial'
+  ])
+  .config(function ($routeProvider, $mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+      .dark();
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name miPromoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the miPromoApp
+ */
+angular.module('miPromoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
